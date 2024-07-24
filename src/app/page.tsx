163e7f2bac682a3dcd4fdb77f26a6366369f4dda
IkuakiR <linyuzhao3@gmail.com>
2024-07-24 19:13:24 +0900

    test-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+import Image from "next/image";
+import styles from "../styles/page.module.scss";
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.icon}></div>
+                <div className={styles.profileText}>
+                    <p>諏訪内有人</p>
+                </div>
+            </div>
+
+            <div className={styles.nav}>
+                <ul>
+                    <li><a href="">ステータス</a></li>
+                    <li><a href="">メディア</a></li>
+                    <li><a href="">投稿</a></li>
+                    <li><a href="">いいね</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
